abf68225d21d2d85473b8840165d6a7301f3e2e7
Stefan <tanaskos@sheridancollege.ca>
2021-03-11 16:41:42 -0500

    Finished Race to Top and added Homepage for teacher.

--- mathwiz_app/lib/widgets/answer_question.dart
+++ mathwiz_app/lib/widgets/answer_question.dart
@@ -1,13 +1,16 @@
 class TrivaQuestionAnswer {
   TrivaQuestionAnswer({
+    this.id,
     this.isExpanded = false,
     this.question,
-    this.answers
+    this.answers,
+    this.answersAmount
+
 
   });
+  int id;
   List<String> answers;
   String question;
   bool isExpanded;
-
-  
+  int answersAmount;
 }
