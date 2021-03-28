c94563416aa7ecfbb3e6996c737bda515e45979d
Viktor <viktorlampert97@gmail.com>
2021-03-28 03:59:58 -0400

    Image grabbing functionality

---
+++ mathwiz_app/lib/views/homework/create_homework.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:ui' as ui;
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:mathwiz_app/views/homework/publish_homework.dart';
+
+import '../../constants.dart';
+
+class CreateHomeworkScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CreateHomeworkScreenState();
+  }
+}
+
+class _CreateHomeworkScreenState extends State<CreateHomeworkScreen> {
+  File _image;
+ // FirebaseVisionTextDetector detector = FirebaseVisionTextDetector.instance;
+  String _text = '';
+  final picker = ImagePicker();
+  Future getImage() async{
+    var pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = File(pickedFile.path);
+
+    });
+
+  }
+
+  Future scanText() async{
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(_image);
+    TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+    final VisionText visionText = await textRecognizer.processImage(visionImage);
+
+    String text = visionText.text;
+    print(text);
+    for (TextBlock block in visionText.blocks){
+      final Rect boundingBox = block.boundingBox;
+      final List<Offset> cornerPoints = block.cornerPoints;
+      final String text = block.text;
+      final List<RecognizedLanguage> languages = block.recognizedLanguages;
+
+      for (TextLine line in block.lines) {
+        for (TextElement element in line.elements){
+          _text += element.text;
+        }
+      }
+    }
+    print(_text);
+    if (_text != ""){
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => PublishHomeworkScreen(_text)));
+    }
+  }
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    ui.Size size = MediaQuery.of(context)
+        .size;// provides total hieght and width of screen
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Create Homework",
+            style: TextStyle(
+              fontSize: 20,
+            )),
+        backgroundColor: kPrimaryColor,
+      ),
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Center(
+                child:Text("Click below to capture image"),
+              ),
+              IconButton(
+                iconSize: 100,
+                icon: Icon(Icons.add_a_photo),
+                onPressed: getImage),
+              ElevatedButton(
+                child:Text("Scan"),
+                onPressed: () {
+                  scanText();
+                },
+              ),
+              _image != null ?
+              Image.asset(_image.path) : Container(),
+              Text(_text),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- mathwiz_app/lib/views/homework/publish_homework.dart
+++ mathwiz_app/lib/views/homework/publish_homework.dart
@@ -1,13 +1,12 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
-import 'package:image_picker/image_picker.dart';
 import 'package:mathwiz_app/widgets/box_button.dart';
 import 'package:mathwiz_app/widgets/box_input_field.dart';
 import '../../constants.dart';
-import 'package:http/http.dart' as http;
 
 class PublishHomeworkScreen extends StatefulWidget {
+  final String text;
+  PublishHomeworkScreen(this.text);
   @override
   State<StatefulWidget> createState() {
     return _PublishHomeworkScreenState();
@@ -15,37 +14,13 @@ class PublishHomeworkScreen extends StatefulWidget {
 }
 
 class _PublishHomeworkScreenState extends State<PublishHomeworkScreen> {
-  File _image;
-  final picker = ImagePicker();
-
-  Future getImage() async{
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
-
-    setState(() {
-      _image = File(pickedFile.path);
-    });
-    getData();
-  }
-
-  Future getData() async{
-    var response = await http.post(
-      Uri.https("https://mathwizocr.cognitiveservices.azure.com", _image.path),
-      headers: {
-        "key": "d8c7258c660d4ae5b24d352e66e240c2",
-        "Accept": "application/json"
-      }
-    );
-    print(response.body);
-  }
-  
-  String _textValue = " ";
   @override
   Widget build(BuildContext context) {
     ui.Size size = MediaQuery.of(context)
         .size;// provides total hieght and width of screen
     return Scaffold(
       appBar: AppBar(
-        title: Text("Create Homework",
+        title: Text("Publish Homework",
             style: TextStyle(
               fontSize: 20,
             )),
@@ -56,14 +31,7 @@ class _PublishHomeworkScreenState extends State<PublishHomeworkScreen> {
           alignment: Alignment.center,
           child: Column(
             children: [
-              Center(
-                child:Text("Click below to capture image")
-              ),
-              IconButton(
-                  iconSize: 100,
-                  icon: Icon(Icons.add_a_photo),
-                  onPressed: getImage),
-              Text(_textValue),
+              SelectableText(widget.text.isEmpty ? 'No text found': widget.text),
               BoxInputFeild(
                 hintText: "Title",
                 icon: Icons.title,
