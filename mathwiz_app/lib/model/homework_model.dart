be9f1c9793c80957e640ad6bb880d48690d573b2
Viktor <viktorlampert97@gmail.com>
2021-04-14 14:14:22 -0400

    Added homework model/publish homework controller

---
+++ mathwiz_app/lib/model/homework_model.dart
@@ -0,0 +1,13 @@
+class HomeworkModel {
+  final String ocrtext;
+  final String title;
+  final int coins;
+  final String description;
+
+ HomeworkModel({
+    this.ocrtext,
+    this.title,
+    this.coins,
+    this.description,
+  });
+}
