cd478384653b235aaab74dce1f2f20273ba4a5dc
Viktor <viktorlampert97@gmail.com>
2021-10-29 17:22:00 -0400

    Completed homework displays all homework, photos can be enlarged. TODO: ability to download files to phone storage

---
+++ mathwiz_app/lib/views/imagepage/image_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:mathwiz_app/model/firebasefile_model.dart';
+import 'package:mathwiz_app/views/homework/completed_homework.dart';
+
+class ImagePage extends StatelessWidget {
+  final FirebaseFile file;
+
+  const ImagePage({
+    Key key,
+    this.file,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text(file.name),
+    centerTitle: true,
+    actions: [
+      IconButton(icon: Icon(Icons.file_download),
+      onPressed: () async {
+        await CompletedHomeworkScreen.downloadFile(file.ref);
+
+        final snackBar = SnackBar(content: Text('Downloaded ${file.name}'),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
+      ),
+      const SizedBox(width: 12),
+    ],
+    ),
+    body:Image.network(file.url,
+    height: double.infinity,
+    width: double.infinity,
+    fit: BoxFit.cover,
+    ),
+);
+  }
+}
